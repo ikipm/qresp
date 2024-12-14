a227b53ab1db13bdc87e3b93bd827b1dd0474d81
RogeRegoR900 <roger.bitgal@gmail.com>
2024-12-14 11:35:11 +0100

    nou canvi

--- src/pacient.js
+++ src/pacient.js
@@ -5,5 +5,36 @@ class Pacient {
         this.cognom1 = cognom1;
         this.cognom2 = cognom2;
         this.edat = edat;
+
+        this.dadesPacient = {
+            PresenciaDeFebre: false,
+            ofeg: false,
+            IncrementMucositatICongestioNasalDolorDeGola: false,
+            IncrementMucositatIFebre: false,
+            DolorToracic: false,
+            Xiulets: false,
+            
+            SignesAlarmaPresents: {
+                FebreAltaODesaturacio: false,
+                IncrementDeRespiracions: false, // representa que aquest valor és true quan >=19 respiracions per minut
+                OfegEnReposOCianosi: false
+            },
+
+            malaltiesPrevis: false,
+            altresCròniques: false,
+
+            medicacioHabit: {
+                antifibrotics: false,
+                immunosupressors: false,
+                oxigenoterapia: false
+            },
+
+            habitsToxics: {
+                consumTabac: false,
+                exposicioFum: false
+            },
+
+            antacadents: false
+        };
     }
 }
