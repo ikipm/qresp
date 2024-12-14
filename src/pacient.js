553b097ac4ef0ff37c741bb273ee7c7943314d2e
RogeRegoR900 <roger.bitgal@gmail.com>
2024-12-14 11:47:55 +0100

    nou pacient

--- src/pacient.js
+++ src/pacient.js
@@ -1,40 +1,44 @@
+class DadesPacient {
+    constructor() {
+        this.PresenciaDeFebre = null;
+        this.ofeg = null;
+        this.IncrementMucositatICongestioNasalDolorDeGola = null;
+        this.IncrementMucositatIFebre = null;
+        this.DolorToracic = null;
+        this.Xiulets = null;
+
+        this.SignesAlarmaPresents = {
+            FebreAltaODesaturacio: null,
+            IncrementDeRespiracions: null, // representa que aquest valor és true quan >=19 respiracions per minut
+            OfegEnReposOCianosi: null
+        };
+
+        this.malaltiesPrevis = null;
+        this.altresCròniques = null;
+
+        this.medicacioHabit = {
+            antifibrotics: null,
+            immunosupressors: null,
+            oxigenoterapia: null
+        };
+
+        this.habitsToxics = {
+            consumTabac: null,
+            exposicioFum: null
+        };
+
+        this.antacadents = null;
+        this.edat = null;
+    }
+}
+
 class Pacient {
-    constructor(id = null, name, cognom1, cognom2 = null, edat) {
+    constructor(id = null, name, cognom1, cognom2 = null, dadesPacient = new DadesPacient()) {
         this.id = id;
         this.name = name;
         this.cognom1 = cognom1;
         this.cognom2 = cognom2;
-        this.edat = edat;
-
-        this.dadesPacient = {
-            PresenciaDeFebre: false,
-            ofeg: false,
-            IncrementMucositatICongestioNasalDolorDeGola: false,
-            IncrementMucositatIFebre: false,
-            DolorToracic: false,
-            Xiulets: false,
-            
-            SignesAlarmaPresents: {
-                FebreAltaODesaturacio: false,
-                IncrementDeRespiracions: false, // representa que aquest valor és true quan >=19 respiracions per minut
-                OfegEnReposOCianosi: false
-            },
-
-            malaltiesPrevis: false,
-            altresCròniques: false,
-
-            medicacioHabit: {
-                antifibrotics: false,
-                immunosupressors: false,
-                oxigenoterapia: false
-            },
-
-            habitsToxics: {
-                consumTabac: false,
-                exposicioFum: false
-            },
-
-            antacadents: false
-        };
+        this.dadesPacient = dadesPacient;
     }
 }
+
