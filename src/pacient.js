91e5506fc50c58052483ddaa9c666f09e1486cfe
RogeRegoR900 <roger.bitgal@gmail.com>
2024-12-15 10:02:07 +0100

    provapacient dins classe

--- src/pacient.js
+++ src/pacient.js
@@ -40,7 +40,7 @@ class DadesPacient {
 }
 
 class Pacient {
-    constructor(id = null, name = '', cognom1 = '', cognom2 = null, enfermetat = '', edat = null, estatActual = null, metge = '', centreSanitari = '', dadesPacient = new DadesPacient()) {
+    constructor(id = null, name = '', cognom1 = '', cognom2 = null, enfermetat = '', edat = null, estatActual = null, metge = '', centreSanitari = '', dadesPacient = new DadesPacient(), provesPacient = new  ProvesPacient()) {
         this.id = id;
         this.name = name;
         this.cognom1 = cognom1;
@@ -51,11 +51,37 @@ class Pacient {
         this.metge = metge;
         this.centreSanitari = centreSanitari;
         this.dadesPacient = dadesPacient;
+        this.provesPacient = provesPacient;
     }
 
     // Consultora per omplir les dadesPacient
     omplirDadesPacient(dades) {
         Object.assign(this.dadesPacient, dades);
+    }
+}
+
+class ProvesPacient {
+    constructor() {
+        this.urgencia = false; // Si es tracta d'una urgència
+        this.provesAnalitiquesUrgents = {
+            rxTorax: false, // Si s'ha fet una radiografia de tòrax
+            suspitapneu: false, // Si hi ha sospita de pneumònia
+        };
+        this.dxConcret = null; // Diagnòstic concret ("pneumonia", "altre" o null)
+        this.sospitaTEP = false; // Si hi ha sospita de TEP
+        this.tepConfirmat = false; // Si s'ha confirmat el TEP
+        this.immunocompetent = false; // Si el pacient és immunocompetent
+        this.immunosupressors = false; // Si el pacient és immunodeprimit
+        this.pcrPositivaInfluenza = false; // Si la PCR és positiva per Influenza
+        this.sospitaCMV = false; // Si hi ha sospita de CMV
+        this.sospitaPneumocystis = false; // Si hi ha sospita de Pneumocystis jirovecii
+        this.necessitaOxigenoterapia = false; // Si requereix oxigenoteràpia
+        this.inhibidorBombaProtons = false; // Si necessita inhibidor de bomba de protons
+        this.nAcetilcisteina = false; // Si necessita N-Acetilcisteïna
+        this.nebulitzacions = false; // Si necessita nebulitzacions
+        this.hbpmNecessari = false; // Si requereix HBPM
+        this.metilprednisolona = false; // Si requereix Metilprednisolona
+        this.losartanNecessari = false; // Si requereix Losartan
     }
 }
 
